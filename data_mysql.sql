939642173192149d30018248fd2844383ef55059
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2022-03-13 20:58:51 +0100

    Initialisation schéma BD et données test

---
+++ data_mysql.sql
@@ -0,0 +1,62 @@
+/* -------------------------------------------------------------------------- */
+/*                               Données de test                              */
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------------ Utilisateurs ------------------------------ */
+
+INSERT INTO Utilisateur VALUES (1, 'user', 'user@user.com', '', 'Jean', 'Dupont', NULL, DEFAULT, 'CLAIR', 0);
+INSERT INTO Utilisateur VALUES (2, 'admin', 'admin@admin.com', '', 'Fabrice', 'Pierre', '2.png', DEFAULT, 'SOMBRE', 1);
+
+/* ---------------------------------- Cours --------------------------------- */
+
+INSERT INTO Cours VALUES (1, 'Découvrir C++', 'Découvrir le C++ avec ce cours.', NULL, 10, 'DEBUTANT', 'PROGRAMMATION', DEFAULT, 'DIAPO');
+INSERT INTO CoursDiapo VALUES (1, '1.ppt');
+
+INSERT INTO Cours VALUES (2, 'Maîtriser C++', 'Maîtriser le C++ avec ce cours.', NULL, 15, 'INTERMEDIAIRE', 'PROGRAMMATION', DEFAULT, 'TEXTE');
+INSERT INTO CoursTexte VALUES (2, '2.pdf');
+
+INSERT INTO Cours VALUES (3, 'Expert C++', 'Devenir expert en C++ avec ce cours.', NULL, 20.5, 'AVANCE', 'PROGRAMMATION', DEFAULT, 'VIDEO');
+INSERT INTO CoursVideo VALUES (3);
+INSERT INTO CoursVideosUrl VALUES (3, 'https://youtu.be/vLnPwxZdW4Y', 1), (3, 'https://youtu.be/zuegQmMdy8M', 2);
+
+INSERT INTO Cours VALUES (4, 'Découvrir JS', 'Découvrir le JS avec ce cours', '4.png', NULL, 'DEBUTANT', 'WEB', DEFAULT, 'TEXTE');
+INSERT INTO CoursTexte VALUES (4, '4.pdf');
+
+/* ----------------------------------- QCM ---------------------------------- */
+
+INSERT INTO QCM VALUES (1, 'QCM C++', 'PROGRAMMATION', 'Testez-vous sur le C++', DEFAULT, '1.xml');
+INSERT INTO QuestionQCM VALUES (1, 1, 'Question 1 : blabla... (bonne réponse : oui)', 'CHOIX_UNIQUE');
+INSERT INTO QuestionChoixUnique VALUES (1);
+INSERT INTO ChoixQuestion VALUES (1, 1, 'oui', 1, 1);
+INSERT INTO ChoixQuestion VALUES (2, 1, 'non', 0, 0);
+INSERT INTO QuestionQCM VALUES (2, 1, 'Question 2 : blabla... (bonne réponse : 1 et 3)', 'CHOIX_MULTIPLES');
+INSERT INTO QuestionChoixMultiples VALUES (2);
+INSERT INTO ChoixQuestion VALUES (3, 2, '1', 1, 0.5);
+INSERT INTO ChoixQuestion VALUES (4, 2, '2', 0, 0);
+INSERT INTO ChoixQuestion VALUES (5, 2, '3', 1, 0.5);
+
+INSERT INTO QCM VALUES (2, 'QCM JS', 'WEB', 'Testez-vous sur le JS', DEFAULT, '2.xml');
+INSERT INTO QuestionQCM VALUES (3, 2, 'Question 1 : blabla... (bonne réponse : "test")', 'SAISIE');
+INSERT INTO QuestionSaisie VALUES (3, 'Saisir la réponse...', 'test', 1);
+
+INSERT INTO QCM VALUES (3, 'QCM JS (2)', 'WEB', 'Descriptif du QCM...', DEFAULT, '3.xml');
+
+INSERT INTO CoursRecommandeQCM VALUES (1, 1, 0, 9);
+INSERT INTO CoursRecommandeQCM VALUES (1, 2, 10, 14);
+INSERT INTO CoursRecommandeQCM VALUES (1, 3, 15, 20);
+INSERT INTO CoursRecommandeQCM VALUES (2, 4, 0, 20);
+INSERT INTO CoursRecommandeQCM VALUES (3, 4, 0, 20);
+
+/* --------------------------- Tentative de cours --------------------------- */
+
+INSERT INTO TentativeCours VALUES (1, 4, 0, DEFAULT, NULL);
+INSERT INTO UtilisateurTentativesCours VALUES (1, 1);
+INSERT INTO TentativeCours VALUES (2, 1, 1, DEFAULT, '2023-01-01 00:00:00');
+INSERT INTO UtilisateurTentativesCours VALUES (2, 1);
+
+/* ---------------------------- Tentative de QCM ---------------------------- */
+
+INSERT INTO TentativeQCM VALUES (1, 2, NULL, 0, 0, DEFAULT, NULL, 0);
+INSERT INTO UtilisateurTentativesQCM VALUES (1, 2);
+INSERT INTO TentativeQCM VALUES (2, 1, 12, 2, 1, DEFAULT, '2023-01-01 00:00:00', 1);
+INSERT INTO UtilisateurTentativesQCM VALUES (2, 2);
---
+++ init_mysql.sql
@@ -0,0 +1,150 @@
+/* -------------------------------------------------------------------------- */
+/*                        Schéma de la base de données                        */
+/* -------------------------------------------------------------------------- */
+
+DROP DATABASE IF EXISTS daw;
+
+CREATE DATABASE daw;
+
+USE daw;
+
+CREATE TABLE Utilisateur (
+  id            INT AUTO_INCREMENT PRIMARY KEY,
+  pseudo        VARCHAR(128) NOT NULL UNIQUE,
+  email         VARCHAR(128) NOT NULL UNIQUE,
+  passHash      VARCHAR(256) NOT NULL,
+  prenom        VARCHAR(128) NOT NULL,
+  nom           VARCHAR(128) NOT NULL,
+  imageUrl      VARCHAR(1024),
+  dateCreation  DATETIME DEFAULT CURRENT_TIMESTAMP,
+  theme         ENUM('CLAIR', 'SOMBRE') NOT NULL DEFAULT 'CLAIR',
+  isAdmin       BOOLEAN NOT NULL DEFAULT 0
+);
+
+CREATE TABLE Cours (
+  id                INT AUTO_INCREMENT PRIMARY KEY,
+  titre             VARCHAR(128) NOT NULL,
+  `description`     TEXT,
+  imageUrl          VARCHAR(1024),
+  tempsMoyen        FLOAT DEFAULT 0,
+  niveauRecommandé  ENUM('DEBUTANT', 'INTERMEDIAIRE', 'AVANCE') NOT NULL DEFAULT 'DEBUTANT',
+  categorie         ENUM('AUCUNE', 'PROGRAMMATION', 'WEB') NOT NULL DEFAULT 'AUCUNE',
+  dateCreation      DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `format`          ENUM('DIAPO', 'VIDEO', 'TEXTE') NOT NULL
+);
+
+CREATE TABLE CoursDiapo (
+  idCours     INT PRIMARY KEY,
+  fichierUrl  VARCHAR(1024) NOT NULL,
+  CONSTRAINT FK_CoursDiapo_Cours FOREIGN KEY (idCours) REFERENCES Cours(id) ON DELETE CASCADE
+);
+
+CREATE TABLE CoursTexte (
+  idCours     INT PRIMARY KEY,
+  fichierUrl  VARCHAR(1024) NOT NULL,
+  CONSTRAINT FK_CoursTexte_Cours FOREIGN KEY (idCours) REFERENCES Cours(id) ON DELETE CASCADE
+);
+
+CREATE TABLE CoursVideo (
+  idCours INT PRIMARY KEY,
+  CONSTRAINT FK_CoursVideo_Cours FOREIGN KEY (idCours) REFERENCES Cours(id) ON DELETE CASCADE
+);
+
+CREATE TABLE CoursVideosUrl (
+  idCours   INT,
+  videoUrl  VARCHAR(1024) NOT NULL,
+  ordre     SMALLINT NOT NULL,
+  PRIMARY KEY (idCours, videoUrl),
+  CONSTRAINT FK_CoursVideosUrl_Cours FOREIGN KEY (idCours) REFERENCES Cours(id) ON DELETE CASCADE
+);
+
+CREATE TABLE QCM (
+  id            INT AUTO_INCREMENT PRIMARY KEY,
+  titre         VARCHAR(128) NOT NULL,
+  categorie     ENUM('AUCUNE', 'PROGRAMMATION', 'WEB') NOT NULL DEFAULT 'AUCUNE',
+  `description` TEXT,
+  dateCreation  DATETIME DEFAULT CURRENT_TIMESTAMP,
+  xmlUrl        VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE CoursRecommandeQCM (
+  idQCM   INT,
+  idCours INT,
+  moyMin  FLOAT NOT NULL,
+  moyMax  FLOAT NOT NULL,
+  PRIMARY KEY (idQCM, idCours),
+  CONSTRAINT FK_CoursRecommande_QCM FOREIGN KEY (idQCM) REFERENCES QCM(id) ON DELETE CASCADE,
+  CONSTRAINT FK_CoursRecommande_Cours FOREIGN KEY (idCours) REFERENCES Cours(id) ON DELETE CASCADE
+);
+
+CREATE TABLE QuestionQCM (
+  id        INT AUTO_INCREMENT PRIMARY KEY,
+  idQCM     INT,
+  question  TEXT NOT NULL,
+  `type` ENUM('CHOIX_UNIQUE', 'CHOIX_MULTIPLES', 'SAISIE') NOT NULL,
+  CONSTRAINT FK_QuestionQCM_QCM FOREIGN KEY (idQCM) REFERENCES QCM(id) ON DELETE CASCADE
+);
+
+CREATE TABLE QuestionChoixUnique (
+  idQuestionQCM INT PRIMARY KEY,
+  CONSTRAINT FK_QuestionChoixUnique_Parent FOREIGN KEY (idQuestionQCM) REFERENCES QuestionQCM(id) ON DELETE CASCADE
+);
+
+CREATE TABLE QuestionChoixMultiples (
+  idQuestionQCM INT PRIMARY KEY,
+  CONSTRAINT FK_QuestionChoixMultiples_Parent FOREIGN KEY (idQuestionQCM) REFERENCES QuestionQCM(id) ON DELETE CASCADE
+);
+
+CREATE TABLE QuestionSaisie (
+  idQuestionQCM INT PRIMARY KEY,
+  placeholder   TEXT,
+  bonneReponse  TEXT,
+  points        FLOAT,
+  CONSTRAINT FK_QuestionSaisies_Parent FOREIGN KEY (idQuestionQCM) REFERENCES QuestionQCM(id) ON DELETE CASCADE
+);
+
+CREATE TABLE ChoixQuestion (
+  id          INT AUTO_INCREMENT PRIMARY KEY,
+  idQuestion  INT,
+  intitule    TEXT,
+  isValide    BOOLEAN,
+  points      FLOAT,
+  CONSTRAINT FK_ChoixQuestion_Question FOREIGN KEY (idQuestion) REFERENCES QuestionQCM(id) ON DELETE CASCADE
+);
+
+CREATE TABLE TentativeQCM (
+  id                  INT AUTO_INCREMENT PRIMARY KEY,
+  idQCM               INT,
+  moy                 FLOAT,
+  pointsActuels       FLOAT,
+  isTermine           BOOLEAN,
+  dateCommence        DATETIME DEFAULT CURRENT_TIMESTAMP,
+  dateTermine         DATETIME,
+  numQuestionCourante SMALLINT,
+  CONSTRAINT FK_TentativeQCM_QCM FOREIGN KEY (idQCM) REFERENCES QCM(id) ON DELETE CASCADE
+);
+
+CREATE TABLE UtilisateurTentativesQCM (
+  idTentativeQCM  INT,
+  idUtilisateur   INT,
+  PRIMARY KEY (idTentativeQCM, idUtilisateur),
+  CONSTRAINT FK_UtilisateurTentativesQCM_TentativeQCM FOREIGN KEY (idTentativeQCM) REFERENCES TentativeQCM(id) ON DELETE CASCADE,
+  CONSTRAINT FK_UtilisateurTentativesQCM_Utilisateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur(id) ON DELETE CASCADE
+);
+
+CREATE TABLE TentativeCours (
+  id            INT AUTO_INCREMENT PRIMARY KEY,
+  idCours       INT,
+  isTermine     BOOLEAN,
+  dateCommence  DATETIME DEFAULT CURRENT_TIMESTAMP,
+  dateTermine   DATETIME,
+  CONSTRAINT FK_TentativeCours_Cours FOREIGN KEY (idCours) REFERENCES Cours(id) ON DELETE CASCADE
+);
+
+CREATE TABLE UtilisateurTentativesCours (
+  idTentativeCours  INT,
+  idUtilisateur     INT,
+  PRIMARY KEY (idTentativeCours, idUtilisateur),
+  CONSTRAINT FK_UtilisateurTentativesCours_TentativeCours FOREIGN KEY (idTentativeCours) REFERENCES TentativeCours(id) ON DELETE CASCADE,
+  CONSTRAINT FK_UtilisateurTentativesCours_Utilisateur FOREIGN KEY (idUtilisateur) REFERENCES Utilisateur(id) ON DELETE CASCADE
+);
