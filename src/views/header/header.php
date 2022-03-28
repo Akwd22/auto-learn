5589e7021fd984a927c37ef696764b8174944f80
LilianLeVrai <70632523+LilianLeVrai@users.noreply.github.com>
2022-03-28 17:50:17 +0200

    Update header.php

--- src/views/header/header.php
+++ src/views/header/header.php
@@ -5,8 +5,8 @@ function infoHead ($title, $description, $link_style)
         <title>$title</title>
         <link rel="shortcut icon" href="../images/logoNoir.png"/>
         <link rel="stylesheet" type="text/css" href="$link_style"/>
-        <link rel="stylesheet" type="text/css" href="../../css/fonts.css"/>
-        <link rel="stylesheet" type="text/css" href="../../css/color.css"/>
+        <link rel="stylesheet" type="text/css" href="../../../css/fonts.css"/>
+        <link rel="stylesheet" type="text/css" href="../../../css/color.css"/>
         <link rel="stylesheet" type="text/css" href="../css/button.css"/>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="description" content="$description"/>
