e820fff4888a0207ac2beb5b4301370d773f1322
LilianLeVrai <70632523+LilianLeVrai@users.noreply.github.com>
2022-03-28 17:46:40 +0200

    Update header.php

--- src/views/header/header.php
+++ src/views/header/header.php
@@ -2,17 +2,15 @@
 function infoHead ($title, $description, $link_style)
 {
     $html = <<<HTML
-        <head>
         <title>$title</title>
         <link rel="shortcut icon" href="../images/logoNoir.png"/>
         <link rel="stylesheet" type="text/css" href="$link_style"/>
-        <link rel="stylesheet" type="text/css" href="../css/fonts.css"/>
-        <link rel="stylesheet" type="text/css" href="../css/color.css"/>
+        <link rel="stylesheet" type="text/css" href="../../css/fonts.css"/>
+        <link rel="stylesheet" type="text/css" href="../../css/color.css"/>
         <link rel="stylesheet" type="text/css" href="../css/button.css"/>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="description" content="$description"/>
         <meta name="author" content="DRUET Eddy, GILI Clément, AULOY Rémy, BARBIER Tom, SONVICO Guillaume, MANZANO Lilian" />
-        </head>
 HTML;
 return $html;
 } 
