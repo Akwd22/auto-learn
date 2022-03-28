f1ffb847b5e4236f10aec2fb3dfb8f5afb749d52
LilianLeVrai <70632523+LilianLeVrai@users.noreply.github.com>
2022-03-28 17:45:53 +0200

    Update header.php

--- src/views/header/header.php
+++ src/views/header/header.php
@@ -2,6 +2,7 @@
 function infoHead ($title, $description, $link_style)
 {
     $html = <<<HTML
+        <head>
         <title>$title</title>
         <link rel="shortcut icon" href="../images/logoNoir.png"/>
         <link rel="stylesheet" type="text/css" href="$link_style"/>
@@ -11,6 +12,7 @@ function infoHead ($title, $description, $link_style)
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="description" content="$description"/>
         <meta name="author" content="DRUET Eddy, GILI Clément, AULOY Rémy, BARBIER Tom, SONVICO Guillaume, MANZANO Lilian" />
+        </head>
 HTML;
 return $html;
 } 
