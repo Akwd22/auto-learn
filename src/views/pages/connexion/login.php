3ca3ff1685b4ddd6a8356d1f0ae294b18d0cb363
LilianLeVrai <70632523+LilianLeVrai@users.noreply.github.com>
2022-03-30 01:08:58 +0200

    front
    front pour la page d'inscription, + différents thèmes (fonctionnement automatique sur les préférence du système ou du navigateur) pour la page de connexion, d'inscription, et le footer

--- src/views/pages/connexion/login.php
+++ src/views/pages/connexion/login.php
@@ -9,34 +9,35 @@
     <body>
         <div id="container">
             <div id="centerDiv">           
-            <form action="/controllers/utilisateurControllers/UtilisateurConnexion.php" method="POST">
-                <h1 id="titleForm">S'identifier</h1>
+                <form action="/controllers/utilisateurControllers/UtilisateurConnexion.php" method="POST">
+                    <h1 id="titleForm">S'identifier</h1>
 
-                <div id="errorField"><p id="textError">identifiant ou mot de passe incorrect.</p></div>
+                    <div id="errorField"><p id="textError">Identifiant ou mot de passe incorrect.</p></div>
 
-                <div class="labelsDiv"><label class="labelsForm">Nom d'utilisateur</label></div>
-                <input class="input" type="pseudo" placeholder="Entrer le nom d'utilisateur" name="pseudo" required>
+                    <div class="labelsDiv"><label class="labelsForm" for="pseudo">Nom d'utilisateur</label></div>
+                    <input id="pseudo" class="input" type="pseudo" placeholder="Entrer le nom d'utilisateur" name="pseudo" required>
 
-                <div class="labelsDiv"><label class="labelsForm">Mot de passe</label></div>
-                <input class="input" type="password" placeholder="Entrer le mot de passe" name="password" required>
+                    <div class="labelsDiv"><label class="labelsForm" for="password">Mot de passe</label></div>
+                    <input id="password" class="input" type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                <p id="linkForgetPassword"><a href=""  class="links">Mot de passe oublié ?</a><p>
+                    <p id="linkForgetPassword"><a href=""  class="links">Mot de passe oublié ?</a><p>
 
-                <input type="submit" id='submit' value="S'identifier" >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
-                }
-                ?>
-            </form>
+                    <input type="submit" id='submit' value="S'identifier" >
+                    <?php
+                    if(isset($_GET['erreur'])){
+                        $err = $_GET['erreur'];
+                        if($err==1 || $err==2)
+                            echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+                    }
+                    ?>
+                </form>
 
-            <div>
-            <p id="text">
-                Vous n'avez pas de compte ? 
-                <a href="" class="links">Créer un compte</a>
-            </p>
+                <div>
+                <p id="text">
+                    Vous n'avez pas de compte ? 
+                    <a href="" class="links">Créer un compte</a>
+                </p>
+                </div>
             </div> 
         </div>
 
--- src/views/pages/inscription/signin.php
+++ src/views/pages/inscription/signin.php
@@ -1,32 +1,52 @@
+<?php require '../../components/header/header.php'; ?>
+
+
 <html>
     <head>
-       <meta charset="utf-8">
+        <?php infoHead('S\'inscrire','Inscris toi pour une meilleur expérience', '../inscription/signin.css');?>
     </head>
     <body>
-        <div id="container">           
-            <form action="/controllers/utilisateurControllers/UtilisateurInscription.php" method="POST">
-                <h1>Inscription</h1>
-                
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="pseudo" placeholder="Entrer le nom d'utilisateur" name="pseudo" required>
+        <div id="container">
+            <div id="centerDiv">              
+                <form action="/controllers/utilisateurControllers/UtilisateurInscription.php" method="POST">
+                    <h1 id="titleForm">Inscription</h1>
+                    <div id="errorField"><p id="textError">Format du mail incorrect.</p></div>
+                    
+                    <div class="labelsDiv"><label class="labelsForm" for="email">Email</label></div>
+                    <input id="email" class="input" type="email" placeholder="Entrer l'email" name="email" required>
 
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+                    <div class="labelsDiv"><label class="labelsForm" for="pseudo">Nom d'utilisateur</label></div>
+                    <input id="pseudo" class="input" type="pseudo" placeholder="Entrer le nom d'utilisateur" name="pseudo" required>
 
-                <label><b>Email</b></label>
-                <input type="email" placeholder="Entrer l'email" name="email" required>
+                    <div class="labelsDiv"><label class="labelsForm" for="password">Mot de passe</label></div>
+                    <input id="password" class="input" type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                <input type="submit" id='submit' value="S'INSCRIRE" >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1)
-                        echo "<p style='color:red'>Utilisateur déjà existant</p>";
-                    if($err==2)
-                        echo  "<p style='color:red'>Email déjà existant</p>";
-                }
-                ?>
-            </form>
+                    
+                    <div id="divCheckbox">
+                        <input id="acceptConditions" type="checkbox" names="acceptConditions" required>
+                    </div>
+                    <div id="divLabelCheckbox">
+                        <label id="labelAcceptConditions" for="acceptConditions">J'ai lu et j'accepte les termes d'utilisation ainsi que la politique de confidentialité.</label>
+                    </div>
+                    
+                    <input type="submit" id='submit' value="S'INSCRIRE" >
+                    <?php
+                    if(isset($_GET['erreur'])){
+                        $err = $_GET['erreur'];
+                        if($err==1)
+                            echo "<p style='color:red'>Utilisateur déjà existant</p>";
+                        if($err==2)
+                            echo  "<p style='color:red'>Email déjà existant</p>";
+                    }
+                    ?>
+                </form>
+                <div>
+                <p id="text">
+                    Vous avez déjà un compte ? 
+                    <a href="" class="links">Se connecter</a>
+                </p>
+                </div> 
+            </div>
         </div>
     </body>
 </html>
