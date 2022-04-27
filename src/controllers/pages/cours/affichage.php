6e8213687c0a12f8e67fdef301edbad98df0bb4f
gs2142 <guillaume.sonvico@hotmail.fr>
2022-04-27 18:23:48 +0200

    controller affichage cours complété

--- src/controllers/pages/cours/affichage.php
+++ src/controllers/pages/cours/affichage.php
@@ -2,7 +2,12 @@
 require_once("databases/SessionManagement.php");
 require_once("databases/CoursCRUD.php");
 require_once("controllers/utils.php");
-require_once "views/pages/cours/creation-modification/creation-modification.php";
+require_once("databases/UtilisateurCRUD.php");
+require_once("models/Cours.php");
+require_once("models/TentativeCours.php");
+require_once("views/pages/cours/affichage.php");
+
+
 
 SessionManagement::session_start();
 
@@ -16,16 +21,29 @@ if (!$coursId) die("ID du cours non spécifié.");
 // Vérification connexion.
 if (!$isLogged) die("Vous devez être connecté");
 
-// Effectuer la récupération du cours et l'affichage.
+$userId=SessionManagement::getUserId();
+
+// Effectuer la récupération du cours
 $conn = new DatabaseManagement();
 $coursCRUD = new CoursCRUD($conn);
+$userCRUD = new UtilisateurCRUD($conn);
+$user=$userCRUD->readUserById($userId);
 
-$isEditMode = false;
 $cours = $coursCRUD->readCoursById($coursId);
 if (!$cours) die("Cours n'existe pas.");
 
+if($user->hasCoursTente($coursId)==false){
+   $tenta=new TentativeCours();
+   $tenta->setDateCommence(new DateTime());
+   $tenta->setIsTermine(false);
+   $tenta->setCours($cours);
+
+   $user->addCoursTentes($tenta);
+   $userCRUD->updateUser($user, $userId); 
+}
+
 
 // Affichage de la vue.
-afficherVue($isEditMode, $cours);
+afficherCours($cours);
 
-//redirect("/", "success", "Affichage du cours");
+redirect("/cours/affichage", "success", "Affichage du cours");
