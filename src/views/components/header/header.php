d768828a58c5bea1bafe2d3b69eeeb46667a60f9
Rémy <60816012+Taarjax@users.noreply.github.com>
2022-04-27 12:03:35 +0200

    fix menu header

--- src/views/components/header/header.php
+++ src/views/components/header/header.php
@@ -87,39 +87,9 @@ HTML;
         </div>
     </nav>
     </div>
+    <script src="../views/components/header/header.js"></script>
 HTML;
 
     echo $html;
-    echo '<script>', 'navSlide();', '</script>';
 };
 ?>
-
-<script type="text/JavaScript">
-    const navSlide = () => {
-        const burger = document.querySelector('.img-profil');
-        const nav = document.querySelector('.burger-links');
-        const navLinks = document.querySelectorAll('.burger-links li');
-
-        burger.addEventListener('click', () => {
-            // Calc right position
-            const rect = burger.getBoundingClientRect();
-            const domsize = document.documentElement.clientWidth;
-            const rightSpacing = domsize - (rect.x) - rect.width;
-            nav.style.right = `${rightSpacing}px`;
-
-            //Toggle Nav
-            nav.classList.toggle('nav-active');
-        });
-    }
-
-    window.onresize = () => {
-        const burger = document.querySelector('.img-profil');
-        const nav = document.querySelector('.burger-links');
-
-        const rect = burger.getBoundingClientRect();
-        const domsize = document.documentElement.clientWidth;
-        const rightSpacing = domsize - (rect.x) - rect.width;
-        nav.style.right = `${rightSpacing}px`;
-    }
-    
-</script>
