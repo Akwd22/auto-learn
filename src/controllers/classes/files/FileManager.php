bee80327907c489417f354f0ff19543af5ae5d99
Akwd22 <60796682+Akwd22@users.noreply.github.com>
2022-04-27 16:16:28 +0200

    Fix collision hash des fichiers importés
    Ajout à la fin du hash, le timestamp pour garder l'unicité du hash si deux fichiers importés sont les mêmes.

--- src/controllers/classes/files/FileManager.php
+++ src/controllers/classes/files/FileManager.php
@@ -46,6 +46,6 @@ abstract class FileManager
    */
   public static function getFileHash($filePath)
   {
-    return hash_file("md5", $filePath);
+    return hash_file("md5", $filePath) . "_" . time();
   }
 }
